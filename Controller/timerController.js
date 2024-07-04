1236d7cf0f5b169e479153152fb3a77a2b64ce29
Your Name <you@example.com>
2024-07-04 10:35:34 +0530

    updated code

--- Controller/timerController.js
+++ Controller/timerController.js
@@ -1,11 +1,31 @@
 const express = require("express");
-const TimerModel = require('../Schema/timerModel'); // Adjust the path as needed
+const TimerModel = require("../Schema/timerModel"); // Adjust the path as needed
 
+let remainingTime = 131;
+
+const updateTimer = async () => {
+  try {
+    let timerDoc = await TimerModel.findById("668513f12d91a3f5ae56e44f");
+    remainingTime = timerDoc ? timerDoc.remainingTime : 131;
+    remainingTime--;
+    if (remainingTime < 0) {
+      remainingTime = 131;
+    }
+    await TimerModel.updateOne(
+      { _id: "668513f12d91a3f5ae56e44f" },
+      { $set: { remainingTime } },
+      { upsert: true }
+    );
+    setTimeout(updateTimer, 1000); // Update every second
+  } catch (error) {
+    console.error("Error updating timer:", error);
+  }
+};
 
 exports.GlobalTimer = async (req, res) => {
   try {
     const timerDoc = await TimerModel.findById("668513f12d91a3f5ae56e44f");
-    const remainingTime = timerDoc ? timerDoc.remainingTime : 131;
+    remainingTime = timerDoc ? timerDoc.remainingTime : 131;
     // Calculate remaining time in minutes and seconds for JSON output
     const minutes = Math.floor(remainingTime / 60);
     const seconds = remainingTime % 60;
@@ -19,30 +39,5 @@ exports.GlobalTimer = async (req, res) => {
   }
 };
 
-
-// Function to update remaining time and reset after 2 minutes 10 seconds
-// function updateTimer() {
-//   remainingTime--;
-//   if (remainingTime === 0) {
-//     remainingTime = 131;
-//   }
-//   setTimeout(updateTimer, 1000); // Update every second
-// }
-
-// // Start the timer on server startup
-// updateTimer();
-
-// exports.GlobalTimer = async (req, res) => {
-//   try {
-//     // Calculate remaining time in minutes and seconds for JSON output
-//     const minutes = Math.floor(remainingTime / 60);
-//     const seconds = remainingTime % 60;
-//     const response = {
-//       remainingTime: `${minutes}:${seconds.toString().padStart(2, "0")}`,
-//     };
-//     res.json(response);
-//   } catch (error) {
-//     console.error("Error fetching global timer data:", error);
-//     res.status(500).json({ message: "Error fetching data" });
-//   }
-// };
+// Start the timer on server startup
+updateTimer();
